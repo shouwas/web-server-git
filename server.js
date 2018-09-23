c3a9117dfc28bb0d1a102920fb8c2a930bd95e32
shouwas <said77@videotron.ca>
2018-09-23 17:15:57 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', ()=>{
+
+    return new Date().getFullYear();
+});
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+    res.render('home.hbs', {
+        titlePage : 'Home page',
+        welcomePage : 'Welcome to home page',
+  });
+}); 
+
+  app.get('/about', function(req, res){
+
+    res.render('about.hbs', {
+        title : 'About page ...',
+    });
+  });
+
+
+  app.get('/bad', function(req, res){
+
+    res.send({
+
+        errorMessage : 'Unable to connect'
+    });
+  })
+
+  app.listen(3000, ()=>{
+console.log('connection is stablishing');
+  });
